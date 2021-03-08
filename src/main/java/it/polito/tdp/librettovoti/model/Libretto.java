0f513f971174fca008ff2c261ec428d788cb4e7b
Antonio <Antonio@AntonioPC>
2021-03-08 11:35:08 +0100

    Soluzione punto 1.2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -16,6 +16,60 @@ public class Libretto {
 		this.voti.add(v);
 	}
 	
+/*	public void stampaVotiUguali (int punteggio) {
+		//il libretto stampa da solo i voti
+		System.out.println();
+	}
+	
+	public String votiUguali (int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli
+		//--> solo il nome?
+		//--> tutto il voto.toString()
+	}
+*/
+	
+	public List<Voto> listaVotiUguali (int punteggio){
+		List<Voto> risultato = new ArrayList<>() ;
+		for(Voto v : this.voti) {
+			if(v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		
+		return risultato;
+	}
+	
+	
+	public Libretto votiUguali (int punteggio) {
+		Libretto risultato = new Libretto();
+		for(Voto v : this.voti) {
+			if(v.getVoto() == punteggio) {
+		      //risultato.add(v);
+				risultato.voti.add(v);
+			}
+		}
+		
+		return risultato;
+	}
+	/**
+	 * Ricerca un voto del corso di cui è specificato il nome
+	 * Se il corso non esiste restituisce null
+	 * @param nomeCorso
+	 * @return
+	 */
+	public Voto ricercaCorso(String nomeCorso) {
+		Voto risultato = null;
+		for(Voto v : this.voti) {
+			if(v.getNome().equals(nomeCorso)) {
+				risultato = v;
+				break;
+			}
+		}
+		
+		return risultato;
+	}
+	
 	public String toString () {
 		String s = "";
 		for(Voto v : this.voti) {
